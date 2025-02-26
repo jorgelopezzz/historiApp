060f352be9bfeff421e737df71941ec132eed285
a-egealopez <aleja@172.22.64.229>
2025-02-26 10:26:09 +0100

    Para el merge

--- pds/src/main/java/gui/ventanas/VentanaBloques.java
+++ pds/src/main/java/gui/ventanas/VentanaBloques.java
@@ -5,6 +5,7 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 
 import gui.GestorGUI;
+import gui.componentes.Componente;
 
 @SuppressWarnings("serial")
 public class VentanaBloques extends VentanaMenu {
@@ -20,9 +21,16 @@ public class VentanaBloques extends VentanaMenu {
     
     @Override
     protected void construirPanelMenu() {
+    	
+    	int columnas = 3;
+    	int numElementos = 14;
+    	
+    	int ANCHO_MENU = ANCHO_VENTANA - 50; // Tamaño estimado de scroll
+    	int ALTO_MENU = Componente.ALTO_COMPONENTE*((numElementos+columnas-1)/columnas);
+    	
 		panelMenu = new JPanel();
 		GestorGUI.configurarPanel(panelMenu, new GridLayout(0,3),
-				GestorGUI.getInstancia().getColorClaro(), ANCHO_VENTANA, ALTO_MENU);
+				GestorGUI.getInstancia().getColorClaro(), ANCHO_MENU, ALTO_MENU);
     }
 
 	@Override
