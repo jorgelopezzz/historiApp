73dd408c9b7471a8d5e62b3907339cae80724a1b
jorgelopezzz <jorgelopez.sp1@gmail.com>
2025-03-27 12:56:09 +0100

    Anotación para serialización (closes #22)

--- pds/src/main/java/dominio/tarea/Tarea.java
+++ pds/src/main/java/dominio/tarea/Tarea.java
@@ -2,6 +2,13 @@ package dominio.tarea;
 
 public abstract class Tarea {
 	
+	/* Anotación para herencia JSON:
+	@JsonSubTypes({
+    @JsonSubTypes.Type(value = TareaEscribir.class, name = "escribir"),
+    @JsonSubTypes.Type(value = TareaLeer.class, name = "leer")
+	})
+	 */
+	
 	/* Atributos de información */
 	private String enunciado;
 	
