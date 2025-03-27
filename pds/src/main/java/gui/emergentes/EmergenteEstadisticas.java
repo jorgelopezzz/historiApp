a3a4cabd9d1f73e48cebb33c562c9f970bacec4f
AlfonsoGM03 <alfonso.gimenezm@um.es>
2025-03-27 16:24:11 +0100

    #18
    -Construida la ventana de estadísticas completa a falta de revisión.

--- pds/src/main/java/gui/emergentes/EmergenteEstadisticas.java
+++ pds/src/main/java/gui/emergentes/EmergenteEstadisticas.java
@@ -1,82 +1,113 @@
 package gui.emergentes;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-
 import gui.GestorGUI;
+import java.awt.*;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class EmergenteEstadisticas extends Emergente { 
+public class EmergenteEstadisticas extends Emergente {
 
-	/* Componentes de organización */
-	private JPanel panelEnvolvente;
+    // Dimensiones
+    private static final int ANCHO_VENTANA = 526;
+    private static final int ALTO_VENTANA = 440;
 
-	private JTextArea areaTexto;
+    private static final int ANCHO_PANEL = 486;
+    private static final int ALTO_PANEL = 370;
 
-	/* Dimensiones */
-	private static final int ANCHO_ESTADISTICA = 600;
-	private static final int ALTO_ESTADISTICA = 60;
+    private static final int MARGEN = 10;
+    private static final int MARGEN_INTERNO = 5;
 
-	private static final int ANCHO_CUADRO = 650;
-	private static final int ALTO_CUADRO = 530;
+    private static final int ANCHO_ETIQUETAS = 150;
+    private static final int ANCHO_DATOS = 80;
 
-	private static final int NUMERO_COLUMNAS_TEXTO = 25;
+    private static final int ALTO_ESTATISTICA = 40;
 
+    // Componentes
+    private JPanel panelPrincipal;
+    private JPanel panelEnvolvente;
 
-	public EmergenteEstadisticas(JFrame ventanaMadre) {
-		super("Estadísticas", GestorGUI.getInstancia().getColorClaro(), ventanaMadre);
-		
-	}
+    public EmergenteEstadisticas(JFrame ventanaMadre) {
+        super("Estadísticas de Uso", GestorGUI.getInstancia().getColorBlanco(), ventanaMadre, ANCHO_VENTANA, ALTO_VENTANA);
+    }
 
-	@Override
-	protected void construir() {
-		
-		/* Panel envolvente */
-		panelEnvolvente = new JPanel();
-		GestorGUI.centrarPanel(panelEnvolvente, this);
-		GestorGUI.fijarTamano(ANCHO_CUADRO, ALTO_CUADRO, panelEnvolvente);
+    @Override
+    protected void construir() {
+        // Panel principal
+        panelPrincipal = new JPanel();
+        GestorGUI.configurarPanel(panelPrincipal, new BorderLayout(), false);
+        GestorGUI.centrarPanel(panelPrincipal, this);
 
+        // Construcción del panel envolvente
+        construirPanelEnvolvente();
 
-		GestorGUI.configurarPanel(panelEnvolvente, new BoxLayout(panelEnvolvente, BoxLayout.Y_AXIS), false);
+        // Montaje
+        panelPrincipal.add(panelEnvolvente, BorderLayout.CENTER);
 
-		/* Panel */
-		
+        this.add(panelPrincipal);
+    }
 
-		areaTexto = GestorGUI.crearAreaTexto("", GestorGUI.getInstancia().getFuenteTexto(),
-				GestorGUI.getInstancia().getColorOscuro(), NUMERO_COLUMNAS_TEXTO);	
-		
-		areaTexto.setText("Estadísticas:");
-		areaTexto.revalidate();
+    private void construirPanelEnvolvente() {
+        // panelEnvolvente
+        panelEnvolvente = new JPanel();
+        GestorGUI.configurarPanel(panelEnvolvente, new BoxLayout(panelEnvolvente, BoxLayout.Y_AXIS), 
+                GestorGUI.getInstancia().getColorBlanco());
+        GestorGUI.fijarTamano(ANCHO_PANEL, ALTO_PANEL, panelEnvolvente);
+        panelEnvolvente.setBorder(GestorGUI.bordeTexto("Estadísticas de Uso",
+                GestorGUI.getInstancia().getFuenteTexto(),
+                GestorGUI.getInstancia().getColorOscuro(),
+                GestorGUI.getInstancia().getColorOscuro()));
 
-		panelEnvolvente.add(Box.createVerticalStrut(15));
-		panelEnvolvente.add(areaTexto);
-		panelEnvolvente.add(Box.createVerticalStrut(15));
+        // Añadir margen
+        panelEnvolvente.add(Box.createVerticalStrut(MARGEN));
 
-		construirEstadistica("Cursos completados", 7);
-		construirEstadistica("Bloques completados", 7);
-		construirEstadistica("Puntuación", 7);
-		construirEstadistica("Tiempo de uso", 7);
-		construirEstadistica("Tiempo de uso diario", 7);
-		construirEstadistica("Mejor racha", 7);
+        // Añadir estadísticas
+        panelEnvolvente.add(crearPanelEstadistica("Cursos completados:", "3"));
+        panelEnvolvente.add(Box.createVerticalStrut(MARGEN_INTERNO));
+        panelEnvolvente.add(crearPanelEstadistica("Bloques completados:", "15"));
+        panelEnvolvente.add(Box.createVerticalStrut(MARGEN_INTERNO));
+        panelEnvolvente.add(crearPanelEstadistica("Puntuación:", "1200"));
+        panelEnvolvente.add(Box.createVerticalStrut(MARGEN_INTERNO));
+        panelEnvolvente.add(crearPanelEstadistica("Tiempo de uso:", "80 horas"));
+        panelEnvolvente.add(Box.createVerticalStrut(MARGEN_INTERNO));
+        panelEnvolvente.add(crearPanelEstadistica("Tiempo de uso diario:", "1.5 horas"));
+        panelEnvolvente.add(Box.createVerticalStrut(MARGEN_INTERNO));
+        panelEnvolvente.add(crearPanelEstadistica("Mejor racha:", "5 días"));
+    }
 
-	}
+    private JPanel crearPanelEstadistica(String nombre, String dato) {
+        JPanel panelContenedor = new JPanel();
+        GestorGUI.configurarPanel(panelContenedor, new BoxLayout(panelContenedor, BoxLayout.Y_AXIS), false);
+        panelContenedor.setBorder(BorderFactory.createLineBorder(GestorGUI.getInstancia().getColorOscuro()));
 
-	private void construirEstadistica(String estadistica, int valor){
-		JPanel panel = new JPanel();
-		GestorGUI.configurarPanel(panel, new BoxLayout(panel, BoxLayout.X_AXIS), GestorGUI.getInstancia().getColorBlanco(), ANCHO_ESTADISTICA, ALTO_ESTADISTICA);
+        // Panel interno para la estadística
+        JPanel panel = new JPanel();
+        GestorGUI.configurarPanel(panel, new BoxLayout(panel, BoxLayout.X_AXIS), false);
+        GestorGUI.fijarTamano(ANCHO_PANEL - 2 * (MARGEN + MARGEN_INTERNO), ALTO_ESTATISTICA, panel);
 
-		JTextArea text = GestorGUI.crearAreaTexto("", GestorGUI.getInstancia().getFuenteTexto(),
-		GestorGUI.getInstancia().getColorOscuro(), NUMERO_COLUMNAS_TEXTO);
-		
-		text.setText(estadistica + " :     " + Integer.toString(valor));
+        panel.add(Box.createHorizontalStrut(MARGEN_INTERNO));
 
-		panel.add(Box.createVerticalStrut(10));
-		panel.add(text);
-		panelEnvolvente.add(panel);
-		panelEnvolvente.add(Box.createVerticalStrut(15));
-	}
+        JLabel lblNombre = new JLabel(nombre);
+        GestorGUI.configurarEtiqueta(lblNombre, false, GestorGUI.getInstancia().getColorOscuro(), 
+                GestorGUI.hacerNegrita(GestorGUI.getInstancia().getFuenteTexto()));
+        GestorGUI.fijarTamano(ANCHO_ETIQUETAS, ALTO_ESTATISTICA, lblNombre);
+        panel.add(lblNombre);
 
+        panel.add(Box.createHorizontalGlue()); // Espacio
+
+        JLabel lblDato = new JLabel(dato);
+        GestorGUI.configurarEtiqueta(lblDato, false, GestorGUI.getInstancia().getColorOscuro(),
+                GestorGUI.getInstancia().getFuenteTexto());
+        GestorGUI.fijarTamano(ANCHO_DATOS, ALTO_ESTATISTICA, lblDato);
+        panel.add(lblDato);
+
+        // Margen a la derecha
+        panel.add(Box.createHorizontalStrut(MARGEN_INTERNO));
+
+        // Añadir el panel interno
+        panelContenedor.add(Box.createVerticalStrut(MARGEN_INTERNO));
+        panelContenedor.add(panel);
+        panelContenedor.add(Box.createVerticalStrut(MARGEN_INTERNO));
+
+        return panelContenedor;
+    }
 }
