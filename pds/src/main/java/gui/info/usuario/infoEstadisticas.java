1972c576e4b58bea11e0055ff6ada3f8d9ff5b8a
AlfonsoGM03 <alfonso.gimenezm@um.es>
2025-04-02 13:02:46 +0200

    #35 Paquetes info de las estadísticas.

---
+++ pds/src/main/java/gui/info/usuario/infoEstadisticas.java
@@ -0,0 +1,62 @@
+package gui.info.usuario;
+
+import java.util.Optional;
+
+import gui.info.Info;
+
+public class infoEstadisticas extends Info{
+    
+    private int puntuacion;
+    private int bloquesCompletados;
+    private int cursosCompletados;
+    private String minutosUso;
+    private String minutosUsoDiario;
+    private int maxRacha;
+
+    public infoEstadisticas (String texto, int puntuacion, int bloquesCompletados, int cursosCompletados, int minutosUso, int diasUso, int maxRacha){
+        super(texto);
+        this.puntuacion = puntuacion;
+        this.bloquesCompletados = bloquesCompletados;
+        this.cursosCompletados = cursosCompletados;
+        this.maxRacha = maxRacha;
+
+        int dias = minutosUso / 1440;
+        int horas = (minutosUso % 1440) / 60;
+        int minutos = minutosUso % 60;
+
+        this.minutosUso = String.format("%02d:%02d:%02d", dias, horas, minutos);
+
+        int tiempo = minutosUso/diasUso;
+        horas = tiempo / 60;
+        minutos = tiempo % 60;
+
+        this.minutosUsoDiario = String.format("%02d:%02d", horas, minutos);
+    }
+
+    /* Getters */
+
+    public int getPuntuacion (){
+        return puntuacion;
+    }
+    
+    public int getBloquesCompletados (){
+        return bloquesCompletados;
+    }
+
+    public int getCursosCompletados (){
+        return cursosCompletados;
+    }
+
+    public String getMinutosUso (){
+        return minutosUso;
+    }
+
+    public String getMinutosUsoDiario (){
+        return minutosUsoDiario;
+    }
+
+    public int getMaxRacha (){
+        return maxRacha;
+    }
+
+}
---
+++ pds/src/main/java/gui/info/usuario/infoEstadisticasProfesor.java
@@ -0,0 +1,21 @@
+package gui.info.usuario;
+
+import java.util.Optional;
+
+public class infoEstadisticasProfesor extends infoEstadisticas{
+    
+    private int cursosPublicados;
+
+    public infoEstadisticasProfesor (String texto, int puntuacion, int bloquesCompletados, int cursosCompletados, int minutosUso, int diasUso, int maxRacha, int cursosPublicados){
+        super(texto, puntuacion, bloquesCompletados, cursosCompletados, minutosUso, diasUso, maxRacha);
+        
+        this.cursosPublicados = cursosPublicados;
+
+    }
+
+
+    /* Getters */
+    public int getCursosPublicados (){
+        return cursosPublicados;
+    }
+}
