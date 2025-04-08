7adc0371d8fd4054e2daff4cba4fde5630b23db2
a-egealopez <aleja@192.168.100.222>
2025-04-08 18:51:32 +0200

    Debuggeando JPA (enseguida de hace pull de la rama principal)

--- pds/src/main/java/dominio/usuario/RepositorioUsuarios.java
+++ pds/src/main/java/dominio/usuario/RepositorioUsuarios.java
@@ -20,18 +20,24 @@ public enum RepositorioUsuarios {
     public void agregarUsuario(Usuario usuario) {
         entityManager.getTransaction().begin();
         entityManager.persist(usuario);
+        //entityManager.flush();
         entityManager.getTransaction().commit();
     }
 
     // Método para encontrar un usuario por nombre
     public Usuario encontrarUsuarioPorNombre(String nombre) {
-        return entityManager.find(Usuario.class, nombre);
+        try {
+            return entityManager.createQuery("SELECT u FROM Usuario u WHERE u.nombre = :nombre", Usuario.class)
+                                .setParameter("nombre", nombre)
+                                .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
     // Método para obtener todos los usuarios
     public List<Usuario> obtenerTodosLosUsuarios() {
-        String jpql = "SELECT u FROM Usuario u";
-        TypedQuery<Usuario> query = entityManager.createQuery(jpql, Usuario.class);
+        TypedQuery<Usuario> query = entityManager.createQuery("SELECT u FROM Usuario u", Usuario.class);
         return query.getResultList();
     }
 
